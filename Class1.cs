13236a0c321056e3b6841dcf0ec94cda0d61f132
YOGESH <YOGESH@DESKTOP-B3HSVTG>
2021-07-17 09:24:07 +0530

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace AccesSpace
+{
+    public class Alib
+    {
+      internal  string str = "Soanwaqne";
+       
+        public string disp()
+        {
+            string name = "yogesh";
+            return name;
+        }
+      public   static void disp1()
+        {
+            Console.WriteLine("protected");
+        }
+        public static void disp2()
+        {
+            Console.WriteLine("default");
+        }
+    }
+}
---
+++ Class2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccesSpace
+{
+   public static class Class2
+    {
+        
+        
+            public static bool StartsWithUpper(this string str)
+            {
+                if (string.IsNullOrWhiteSpace(str))
+                    return false;
+
+                char ch = str[0];
+                return char.IsUpper(ch);
+            }
+        }
+    }
+
+
